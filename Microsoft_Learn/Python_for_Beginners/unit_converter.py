65e9f683588ac95e2c62116c2cfeb0b9cf147a9f
Patrick_H <patrick.c.hennessey@gmail.com>
2024-02-27 13:48:45 -0500

    Created unit_converter.py

---
+++ Microsoft_Learn/Python_for_Beginners/unit_converter.py
@@ -0,0 +1,18 @@
+# Today's Date
+from datetime import date
+print(date.today())
+
+# Building a Unit Converter
+
+# Task: Convert 11 parsecs to lightyears
+# Conversion formula: 1 parsec = 3.26 lightyears
+
+# Set parsecs variable value
+parsecs = 11
+# print(parsecs)
+
+# Set lightyears variable value
+lightyears = parsecs * 3.26
+
+# Answer
+print(str(parsecs)+ " parsecs is "+ str(lightyears) + " lightyears")
