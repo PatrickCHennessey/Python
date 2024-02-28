32949d7c5e266fe3c1d19a87bb7d30b454e79c92
Patrick_H <patrick.c.hennessey@gmail.com>
2024-02-28 13:03:36 -0500

    Added example output

---
+++ Microsoft_Learn/Python_for_Beginners/boolean_logic_in_python.py
@@ -0,0 +1,28 @@
+# if test_expression:
+#     statement(s) to be run
+#
+# # Example
+# a = 93
+# b = 27
+# if a >= b:
+#     print(a)
+#
+# Output difference between indented print(b) line
+# Example 1
+# a = 24
+# b = 44
+# if a <= 0:
+#     print(a)
+# print(b)
+# # Output is 44
+#
+#
+# # Example 2
+# a = 24
+# b = 44
+# if a <= 0:
+#     print(a)
+#     print(b)
+# # Output is 0
+
+
