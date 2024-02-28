bf727e238a2c48e50409a53188dd4f25ee9b7e9e
Patrick_H <patrick.c.hennessey@gmail.com>
2024-02-28 14:01:16 -0500

    Added : to end of line 13

--- Microsoft_Learn/Python_for_Beginners/and_or_operators_exercise.py
+++ Microsoft_Learn/Python_for_Beginners/and_or_operators_exercise.py
@@ -10,7 +10,7 @@
 object_size = 10
 proximity = 9000
 
-if object_size > 5 and proximity < 1000
+if object_size > 5 and proximity < 1000:
     print('Evasive maneuvers required')
 else:
     print('Object poses no threat')
