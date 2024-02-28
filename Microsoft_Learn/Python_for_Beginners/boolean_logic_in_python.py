108e5bdb3e8fc785965cb172bcff8725c2a94929
Patrick_H <patrick.c.hennessey@gmail.com>
2024-02-28 13:15:12 -0500

    Added if,elif,else examples

--- Microsoft_Learn/Python_for_Beginners/boolean_logic_in_python.py
+++ Microsoft_Learn/Python_for_Beginners/boolean_logic_in_python.py
@@ -1,13 +1,16 @@
 # if test_expression:
 #     statement(s) to be run
-#
+
 # # Example
 # a = 93
 # b = 27
 # if a >= b:
 #     print(a)
-#
+
+
+
 # Output difference between indented print(b) line
+
 # Example 1
 # a = 24
 # b = 44
@@ -15,8 +18,8 @@
 #     print(a)
 # print(b)
 # # Output is 44
-#
-#
+
+
 # # Example 2
 # a = 24
 # b = 44
@@ -26,3 +29,70 @@
 # # Output is 0
 
 
+# if test_expression:
+#     # statement(s) to be run
+# else:
+#     # statement(s) to be run
+
+# Example
+
+# a = 27
+# b = 93
+# if a >= b:
+#     print(a)
+# else:
+#     print(b)
+
+
+# Use of elif
+
+# if test_expression:
+#     # statement(s) to be run
+# elif test_expression:
+#     # statement(s) to be run
+
+# a = 27
+# b = 93
+# if a <= b:
+#     print("a is less than or equal to b")
+# elif a == b:
+#     print("a is equal to b")
+
+
+# Combine if, elif, and else
+
+# if test_expression:
+#     # statement(s) to be run
+# elif test_expression:
+#     # statement(s) to be run
+# elif test_expression:
+#     # statement(s) to be run
+# else:
+#     # statement(s) to be run
+
+    # a = 27
+    # b = 93
+    # if a < b:
+    #     print("a is less than b")
+    # elif a > b:
+    #     print("a is greater than b")
+    # else:
+    #     print("a is equal to b")
+
+
+# Use of nested if, elif, and else
+
+# if test_expression:
+#     # statement(s) to be run
+#     if test_expression:
+#         # statement(s) to be run
+#     else:
+#         # statement(s) to be run
+# elif test_expression:
+#     # statement(s) to be run
+#     if test_expression:
+#         # statement(s) to be run
+#     else:
+#         # statement(s) to be run
+# else:
+#     # statement(s) to be run
