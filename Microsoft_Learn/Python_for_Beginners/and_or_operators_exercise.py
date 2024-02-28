d37ca62309d3558e1b467b2533c084a779ef188f
Patrick_H <patrick.c.hennessey@gmail.com>
2024-02-28 13:51:09 -0500

    Added exercise directions and variable values

---
+++ Microsoft_Learn/Python_for_Beginners/and_or_operators_exercise.py
@@ -0,0 +1,12 @@
+# Exercise: Test both the object's size and proximity.
+
+# Add the code to the cell below to create two variables: object_size and proximity.
+# Set object_size to 10 to represent 10m3. Set proximity to 9000.
+
+# Add code to display the following message:
+# Evasive maneuvers required if both object_size is greater than 5 and proximity is less than 1000.
+# Otherwise display a message saying Object poses no threat.
+
+object_size = 10
+proximity = 9000
+
