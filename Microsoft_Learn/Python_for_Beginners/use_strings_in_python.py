04fa9fba3156e978ddeef22edd0e0b2ee68dbe95
Patrick_H <patrick.c.hennessey@gmail.com>
2024-02-29 13:56:10 -0500

    Added string, multiline, title

---
+++ Microsoft_Learn/Python_for_Beginners/use_strings_in_python.py
@@ -0,0 +1,29 @@
+# fact = "The Moon has no atmosphere."
+# print(fact)
+
+# # Output: "The Moon has no atmosphere."
+
+# fact = "The Moon has no atmosphere."
+# fact + "No sound can be heard on the Moon."
+# print(fact)
+
+# # Output: The Moon has no atmosphere.
+# # Since the second statement is not defined in the fact variable, it is not included in the output
+
+# fact = "The Moon has no atmosphere."
+# two_facts = fact + "No sound can be heard on the Moon."
+# print(two_facts)
+
+# # Output: The Moon has no atmosphere. No sound can be heard on the Moon.
+
+# multiline = "Facts about the Moon:\n There is no atmosphere.\n There is no sound."
+# print(multiline)
+
+# # \n will indicate that the following string will continue on the next line.
+
+# heading = "temperatures and facts about the moon"
+# heading_upper = heading.title()
+# print(heading_upper)
+
+# # Output: Temperatures And Facts About The Moon
+
