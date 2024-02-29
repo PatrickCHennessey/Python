de92bb9fdaf8cae78db2ff9735f4e2863d736a08
Patrick_H <patrick.c.hennessey@gmail.com>
2024-02-29 14:04:35 -0500

    Added split()

--- Microsoft_Learn/Python_for_Beginners/use_strings_in_python.py
+++ Microsoft_Learn/Python_for_Beginners/use_strings_in_python.py
@@ -16,10 +16,10 @@
 
 # # Output: The Moon has no atmosphere. No sound can be heard on the Moon.
 
-# multiline = "Facts about the Moon:\n There is no atmosphere.\n There is no sound."
-# print(multiline)
+multiline = "Facts about the Moon:\n There is no atmosphere.\n There is no sound."
+print(multiline)
 
-# # \n will indicate that the following string will continue on the next line.
+# \n will indicate that the following string will continue on the next line.
 
 # heading = "temperatures and facts about the moon"
 # heading_upper = heading.title()
@@ -27,3 +27,15 @@
 
 # # Output: Temperatures And Facts About The Moon
 
+# temperatures = "Daylight: 260 F Nighttime: -280 F"
+# temperatures_list = temperatures.split()
+# print(temperatures_list)
+
+# # Resulting output ['Daylight:', '260', 'F', 'Nighttime:', '-280', 'F']
+
+temperatures = "Daylight: 260 F\n Nighttime: -280 F"
+temperatures_list = temperatures.split('\n')
+print(temperatures_list)
+
+# # Defining split('\n') will tell the script to split when it encounters \n.
+# # The resulting output is then ['Daylight: 260 F', 'Nighttime: -280 F']
