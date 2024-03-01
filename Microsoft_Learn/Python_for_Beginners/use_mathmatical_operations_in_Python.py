c9442a1be196886395257db0b6446b04b3953450
Patrick_H <patrick.c.hennessey@gmail.com>
2024-03-01 16:03:38 -0500

    "Added Python formulas for +-*/"

---
+++ Microsoft_Learn/Python_for_Beginners/use_mathmatical_operations_in_Python.py
@@ -0,0 +1,15 @@
+# Addition
+add = 30 + 12
+print(answer)
+
+# Subtraction
+difference = 30 - 12
+print(difference)
+
+# Multiplication
+product = 30 * 12
+print(product)
+
+# Division
+quotient = 30 / 12
+print(quotient)
