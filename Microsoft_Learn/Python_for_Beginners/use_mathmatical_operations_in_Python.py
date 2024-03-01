a4c6d6f55beca22294573345a4e11e2625f6cf1f
Patrick_H <patrick.c.hennessey@gmail.com>
2024-03-01 16:28:17 -0500

    solved for distance_km variable.

--- Microsoft_Learn/Python_for_Beginners/use_mathmatical_operations_in_Python.py
+++ Microsoft_Learn/Python_for_Beginners/use_mathmatical_operations_in_Python.py
@@ -43,3 +43,17 @@ print(str(display_seconds) + ' seconds')
 
 first_planet = 149597870
 second_planet = 778547200
+
+# You have two variables which store the distance between each planet and a common point: the sun.
+
+# Start by adding the code to subtract first_planet from second_planet
+# and store the result in a variable named distance_km. Display the value to the screen.
+
+# Then add the code to convert distance_km to miles by dividing
+# it by 1.609344 (the rough difference between miles and kilometers)
+# and store the result in a variable named distance_mi. Display the value to the screen.
+
+# Subtract these two values to determine the distance between the planets.
+distance_km = second_planet - first_planet
+print(distance_km)
+
