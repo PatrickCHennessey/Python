c8c17d002d04a2e47108beb70a051b48f0b1d493
Patrick_H <patrick.c.hennessey@gmail.com>
2024-03-01 16:19:52 -0500

    Completed seconds converter exercise

--- Microsoft_Learn/Python_for_Beginners/use_mathmatical_operations_in_Python.py
+++ Microsoft_Learn/Python_for_Beginners/use_mathmatical_operations_in_Python.py
@@ -1,20 +1,35 @@
-# Addition
-add = 30 + 12
-print(answer)
+# # Addition
+# add = 30 + 12
+# print(add)
+#
+# # Subtraction
+# difference = 30 - 12
+# print(difference)
+#
+# # Multiplication
+# product = 30 * 12
+# print(product)
+#
+# # Division
+# quotient = 30 / 12
+# print(quotient)
+#
+# # Exercise convert 1042 seconds to minutes and seconds
+# seconds = 1042
+# display_minutes = seconds / 60
+# print(display_minutes)
+#
+# # Output is 17.366666666667, but we want a non-decimal output.
 
-# Subtraction
-difference = 30 - 12
-print(difference)
-
-# Multiplication
-product = 30 * 12
-print(product)
-
-# Division
-quotient = 30 / 12
-print(quotient)
-
-# Exercise convert 1042 seconds to minutes and seconds
 seconds = 1042
-display_minutes = seconds / 60
+display_minutes = seconds // 60 # a second forward slash will have the expression round down.
+print(display_minutes)
 
+# Excellent now we know that 1042 divided by 60 = 17 minutes to find the remainder we incorporate the % modulo operator
+seconds = 1042
+display_seconds = 1042 % 60
+print(display_seconds)
+
+# Converted the variables to a string to add "minutes" and "seconds" after both answers
+print(str(display_minutes) + ' minutes')
+print(str(display_seconds) + ' seconds')
