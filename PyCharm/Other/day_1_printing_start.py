5a38fcd761257321a5aac364c792c9614596fff2
Patrick_H <patrick.c.hennessey@gmail.com>
2024-03-19 18:55:52 -0400

    Finished several exercises

---
+++ PyCharm/Other/day_1_printing_start.py
@@ -0,0 +1,26 @@
+# Hello World Statement
+print("Hello World!")
+
+# Print multiple statements with single print statement
+print("Hello World!\nHello World!\nHello World!")
+
+# Printing multiple steps for a recipe
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+# Adding a quote within a string
+print("6. \"Bake at 210 degrees C for 30 minutes for a firmer cake\" says Betty Crocker.")
+
+# Exercise: Add a space when concatenating two words
+# Undesired Output: HelloPatrick
+# Desired Output: Hello Patrick
+# Method 1
+print("Hello"+" Patrick")
+# Method 2
+print("Hello "+"Patrick")
+# Method 3
+print("Hello"+" "+"Patrick")
+
