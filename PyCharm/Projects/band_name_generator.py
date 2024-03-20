0d1926b0babe9e68c7153845ae1eed7483b5ae4b
Patrick_H <patrick.c.hennessey@gmail.com>
2024-03-20 15:05:34 -0400

    Finished Project Goal 3.

--- PyCharm/Projects/band_name_generator.py
+++ PyCharm/Projects/band_name_generator.py
@@ -7,17 +7,18 @@
 
 # 1. Create a greeting for the program
 greeting = ("Hello! Welcome to the band name generator program."
-            "\nIn order to create a rocking *pause for groans* band name, we'll need some additional information")
+            "\nIn order to create a rocking *pause for groans* band name, we'll need some additional information.")
 print(greeting)
 
 # 2. Ask the user for the city that they grew up in.
-city_name = input("What city did you grow up in? ")
-print(city_name + "\nRight on! That's a great city")
+city_name = input("Question #1: What city did you grow up in? ")
+print(city_name + ", Right on! That's a great city!")
 
 # 3. Ask the user for the name of a pet.
-name_of_pet = input("What is the name of your pet(s) ")
-print(name_of_pet + "\nRight on! That's a great pet name")
+name_of_pet = input("Question #2: What is the name of your pet(s) ")
+print(name_of_pet + ", Right on! That's a great pet name!")
 
 # 4. Combine the name of their city and pet and show them their band name.
+
 
 # 5. Make sure the input cursor shows on a new line:
