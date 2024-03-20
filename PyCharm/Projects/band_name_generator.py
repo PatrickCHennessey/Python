d5b95ace496b9fc0082b5e7548cc81dd4c520580
Patrick_H <patrick.c.hennessey@gmail.com>
2024-03-20 14:49:12 -0400

    Outlined project goals and added greeting.

---
+++ PyCharm/Projects/band_name_generator.py
@@ -0,0 +1,21 @@
+#  Project Goals: Band Name Generator
+#1. Create a greeting for your program.
+#2. Ask the user for the city that they grew up in.
+#3. Ask the user for the name of a pet.
+#4. Combine the name of their city and pet and show them their band name.
+#5. Make sure the input cursor shows on a new line:
+
+#1 Create a greeting for the program
+greeting = ("Hello! Welcome to the band name generator program."
+            "\nIn order to create a rocking *pause for groans* band name, we'll need some additional information")
+print(greeting)
+
+#2 Ask the user for the city that they grew up in.
+
+
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line:
