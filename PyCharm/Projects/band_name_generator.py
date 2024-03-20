3c131f522664feaedde20f404d795fae44b79e16
Patrick_H <patrick.c.hennessey@gmail.com>
2024-03-20 14:57:18 -0400

    Finished second project goal added new line #15.

--- PyCharm/Projects/band_name_generator.py
+++ PyCharm/Projects/band_name_generator.py
@@ -12,9 +12,10 @@ print(greeting)
 
 # 2. Ask the user for the city that they grew up in.
 city_name = input("What city did you grow up in?")
-print(city_name)
+print(city_name + "\nRight on! That's a great city")
 
 # 3. Ask the user for the name of a pet.
+
 
 # 4. Combine the name of their city and pet and show them their band name.
 
